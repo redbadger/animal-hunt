59bcc562dda0662682280e8493ee72fce647eaae
Viktor Charypar <charypar@gmail.com>
2024-01-10 17:59:55 +0000

    Enable universal links in the iOS app

--- ios/AnimalHunt/AnimalHuntApp.swift
+++ ios/AnimalHunt/AnimalHuntApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct AnimalHuntApp: App {
+    private let core: Core
+
+    init() {
+        core = Core()
+    }
+
     var body: some Scene {
         WindowGroup {
-            RootView(core: Core())
+            RootView(core: core).onOpenURL(perform: { url in
+                let url = url.absoluteString
+
+                core.update(.scannedUrl(.url(url)))
+            }).onContinueUserActivity(NSUserActivityTypeBrowsingWeb, perform: { userActivity in
+                guard let url = userActivity.webpageURL?.absoluteString else {
+                    return
+                }
+                
+                core.update(.scannedUrl(.url(url)))
+            })
         }
     }
 }
